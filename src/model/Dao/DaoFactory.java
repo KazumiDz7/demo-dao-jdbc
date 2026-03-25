09ce608410a56a0d31159470eb2334db7217a2c9
KazumiDz7 <fn47062.com>
2026-03-25 20:52:13 -0300

    findById implementation

--- src/model/Dao/DaoFactory.java
+++ src/model/Dao/DaoFactory.java
@@ -1,5 +1,13 @@
 package model.Dao;
 
+import db.DB;
+import model.Dao.impl.SellerDaoJDBC;
+
 public class DaoFactory {
 
+	public static SellerDao creatSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	
 }
